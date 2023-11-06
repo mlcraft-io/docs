f349317fa6040a4b0707ff6abf30195becd8a99b
Ivan Fokeev <vanya@vanya.cc>
2023-11-06 18:26:04 +0300

    fix: docs url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 const organizationName = "mlcraft-io";
 const projectName = "docs";
-const baseUrl = `/${projectName}/`;
+const baseUrl = "/";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
